853c0bf8f55fc09f63b9e27cd5669a7f16e6e3d4
Maciej <100779508+Offsidee@users.noreply.github.com>
2022-03-28 18:04:28 +0200

    Add files via upload

---
+++ php_01_widok_kontroler/app/calc.php
@@ -0,0 +1,89 @@
+<?php
+// KONTROLER strony kalkulatora
+require_once dirname(__FILE__).'/../config.php';
+
+// W kontrolerze niczego nie wysyła się do klienta.
+// Wysłaniem odpowiedzi zajmie się odpowiedni widok.
+// Parametry do widoku przekazujemy przez zmienne.
+
+// 1. pobranie parametrów
+
+$x = $_REQUEST ['x'];
+$y = $_REQUEST ['y'];
+$z = $_REQUEST ['z'];
+
+// 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
+
+// sprawdzenie, czy parametry zostały przekazane
+if ( ! (isset($x) && isset($y) && isset($z))) {
+	//sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+}
+
+// sprawdzenie, czy potrzebne wartości zostały przekazane
+
+if ( $x == "") {
+	$messages [] = 'Nie podano liczby 1';
+}
+if ( $y == "") {
+	$messages [] = 'Nie podano liczby 2';
+}
+if ( $z == "") {
+	$messages [] = 'Nie podano liczby 3';
+}
+
+//nie ma sensu walidować dalej gdy brak parametrów
+if (empty( $messages )) {
+	
+	// sprawdzenie, czy $x i $y i $z są liczbami całkowitymi
+	if (! is_numeric( $x )) {
+		$messages [] = 'Pierwsza wartość nie jest liczbą całkowitą';
+	}
+	
+	if (! is_numeric( $y )) {
+		$messages [] = 'Druga wartość nie jest liczbą całkowitą';
+	}	
+
+	if (! is_numeric( $z )) {
+		$messages [] = 'Trzecia wartość nie jest liczbą całkowitą';
+	}	
+}
+
+// 3. wykonaj zadanie jeśli wszystko w porządku
+
+if (empty ( $messages )) { // gdy brak błędów
+	
+	//konwersja parametrów na int
+	$x = doubleval($x);
+	$y = doubleval($y);
+	$z = doubleval($z);
+	
+	//wykonanie operacji
+/*	switch ($operation) {
+		case 'minus' :
+			$result = $x - $y;
+			break;
+		case 'times' :
+			$result = $x * $y;
+			break;
+		case 'div' :
+			$result = $x / $y;
+			break;
+		default :
+			$result = $x + $y;
+			break;
+	}
+	*/
+
+	$y = $y/100;
+
+	$wynik = $x * $y * $z/365; 
+
+	$result = round($wynik,2) ;
+
+}
+
+// 4. Wywołanie widoku z przekazaniem zmiennych
+// - zainicjowane zmienne ($messages,$x,$y,$operation,$result)
+//   będą dostępne w dołączonym skrypcie
+include 'calc_view.php';
