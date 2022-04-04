12a0244f4359d0620fb1688a6784ab95d9b29963
Maciej <100779508+Offsidee@users.noreply.github.com>
2022-04-04 23:27:35 +0200

    Add files via upload

---
+++ php_06/templates_c/7a4c7e59033dc15d3025fb390f3cace716c4f1b7_0.file.CalcView.tpl.php
@@ -0,0 +1,134 @@
+<?php
+/* Smarty version 3.1.30, created on 2022-04-04 23:20:41
+  from "D:\xampp\htdocs\php_06\app\views\CalcView.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_624b6129b2e398_00132819',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '7a4c7e59033dc15d3025fb390f3cace716c4f1b7' => 
+    array (
+      0 => 'D:\\xampp\\htdocs\\php_06\\app\\views\\CalcView.tpl',
+      1 => 1649107240,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:main.tpl' => 1,
+  ),
+),false)) {
+function content_624b6129b2e398_00132819 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, true);
+?>
+
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_520747637624b6129b1c6b2_34655635', 'footer');
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_93711706624b6129b2de76_39294601', 'content');
+$_smarty_tpl->inheritance->endChild();
+$_smarty_tpl->_subTemplateRender("file:main.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 2, false);
+}
+/* {block 'footer'} */
+class Block_520747637624b6129b1c6b2_34655635 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+przykładowa tresć stopki wpisana do szablonu głównego z szablonu kalkulatora<?php
+}
+}
+/* {/block 'footer'} */
+/* {block 'content'} */
+class Block_93711706624b6129b2de76_39294601 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+
+<h3 >Prosty kalkulator</h3>
+
+
+<form class="pure-form pure-form-stacked" action="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
+calcCompute" method="post">
+	<fieldset>
+		<label for="x">Pierwsza liczba</label>
+		<input id="x" type="text" placeholder="wartość x" name="x" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->x;?>
+">
+					
+		<label for="y">Druga liczba</label>
+		<input id="y" type="text" placeholder="wartość y" name="y" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->y;?>
+">
+
+		<label for="y">Trzecia liczba</label>
+		<input id="z" type="text" placeholder="wartość z" name="z" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->z;?>
+">
+	</fieldset>
+	<button type="submit" class="pure-button pure-button-primary">Oblicz</button>
+</form>
+
+<div class="messages">
+
+
+<?php if ($_smarty_tpl->tpl_vars['msgs']->value->isError()) {?>
+	<h4>Wystąpiły błędy: </h4>
+	<ol class="err">
+	<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['msgs']->value->getErrors(), 'err');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['err']->value) {
+?>
+	<li><?php echo $_smarty_tpl->tpl_vars['err']->value;?>
+</li>
+	<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+	</ol>
+<?php }?>
+
+
+<?php if ($_smarty_tpl->tpl_vars['msgs']->value->isInfo()) {?>
+	<h4>Informacje: </h4>
+	<ol class="inf">
+	<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['msgs']->value->getInfos(), 'inf');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['inf']->value) {
+?>
+	<li><?php echo $_smarty_tpl->tpl_vars['inf']->value;?>
+</li>
+	<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+	</ol>
+<?php }?>
+
+<?php if (isset($_smarty_tpl->tpl_vars['res']->value->result)) {?>
+	<h4>Wynik</h4>
+	<p class="res">
+	<?php echo $_smarty_tpl->tpl_vars['res']->value->result;?>
+
+	</p>
+<?php }?>
+
+</div>
+
+<?php
+}
+}
+/* {/block 'content'} */
+}
