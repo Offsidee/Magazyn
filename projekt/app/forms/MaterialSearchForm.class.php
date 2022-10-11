3a9ca395ae7b43c3cfe53e83dafdab60cf40f22a
Maciej <100779508+Offsidee@users.noreply.github.com>
2022-10-11 17:12:55 +0200

    Delete projekt directory

--- projekt/app/forms/MaterialSearchForm.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace app\forms;
-
-class MaterialSearchForm {
-	public $nazwa;
-} 
--- projekt/app/forms/PracownikSearchForm.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace app\forms;
-
-class PracownikSearchForm {
-	public $nazwisko;
-} 
